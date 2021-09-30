a0ae05d8bb5061833d732eb3cd97bc3d1b640b26
unknown <barbara.cristianefs@gmail.com>
2021-09-30 09:36:00 -0300

    criacao das models user, userlogin, tema e postagem

---
+++ src/app/model/Tema.ts
@@ -0,0 +1,9 @@
+import { Postagem } from "./Postagem"
+
+export class Tema{
+
+public idTema: number
+public tema: string
+public postagens: Postagem[]
+        
+}
---
+++ src/app/model/UserLogin.ts
@@ -0,0 +1,10 @@
+export class UserLogin{
+
+    public idUsuario: number
+    public nome: string
+    public email: string
+    public senha: string
+    public token: string
+    public foto:string
+    public tipo: string
+    }
